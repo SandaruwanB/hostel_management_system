3f7adb494c4c6556e4fae48ddf0b4a4882fd7c29
SandaruwanB <sandarusbandara110@gmail.com>
2024-10-14 20:33:00 +0530

    initialize server correctly

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
-
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config({'path' : '.env'});
 
 const app = express();
 
-app.listen(8000, ()=>{
-    console.log(`server running on http://localhost:8000`);
+const PORT = process.env.PORT || 8000
+
+app.use(cors());
+app.use(express.json());
+app.use('/', require('./app/routes/route'));
+
+app.listen(PORT, ()=>{
+    console.log(`server running on http://localhost:${PORT}`);
 })
