f52da22caf35bd82be3a7b7caba4cf56a033c8e0
SandaruwanB <sandarusbandara110@gmail.com>
2024-11-09 22:52:13 +0530

    [DEV] login form validations

---
+++ public/js/main.js
@@ -0,0 +1,19 @@
+$('#loginForm').submit(function (e) { 
+    e.preventDefault();
+
+    const username = $('#uname').val();
+    const password = $('#password').val();
+
+    if (username && password){
+
+    }
+    else{
+        $("#errorText").text("All fields are required");
+        $("#errorSpace").removeClass("hidden");
+    }
+});
+
+
+function removeAlert(){
+    $('#errorSpace').addClass("hidden");
+}
