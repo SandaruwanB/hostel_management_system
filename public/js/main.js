9ea93b054f9d8b5eab34db360441a54432660b92
SandaruwanB <sandarusbandara110@gmail.com>
2024-12-07 14:25:38 +0530

    [ADD] react toastify and validate

--- public/js/main.js
+++ public/js/main.js
@@ -40,3 +40,35 @@ function removeLoginAlert(){
     $(`#errorSpace`).addClass("hidden");
 }
 
+
+// user functions
+$('#createUser').click(function (e) { 
+    e.preventDefault();
+    const username = $('#username').val();
+    const email = $('#email').val();
+    const role = $('#role').val();
+    const password = $('#password').val();
+    const repassword = $('#repassword').val();
+
+    if (username && email && role && password && repassword){
+
+    }
+    else{
+        showError("All fields are required", "#ff1100");
+    }
+});
+
+function showError(error, color){
+    Toastify({
+        text: `${error}`,
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "center", 
+        stopOnFocus: true,
+        style: {
+            background: `${color}`,
+        },
+        onClick: function(){}
+    }).showToast();
+}
