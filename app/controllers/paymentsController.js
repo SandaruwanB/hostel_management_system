b744613b873e0ba0cf2419e3386a85cf950898eb
SandaruwanB <sandarusbandara110@gmail.com>
2025-01-07 21:08:32 +0530

    [DEV] payment data

--- app/controllers/paymentsController.js
+++ app/controllers/paymentsController.js
@@ -1,5 +1,16 @@
-module.exports.index = (req, res)=>{
-    res.render('user/payments', {user : req.user});
+const payments = require('../models/payments');
+const students = require('../models/students');
+
+module.exports.index = async (req, res)=>{
+    const paymentDetails = await payments.findAll({
+        include : [
+            {
+                model : students,
+                as : "student"
+            }
+        ]
+    });
+    res.render('user/payments', {user : req.user, payments : paymentDetails});
 }
 
 module.exports.getCreateView = (req, res)=>{
