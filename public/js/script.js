c3e949f722b6f746741c4a20da03ce7978964387
SandaruwanB <sandarusbandara110@gmail.com>
2025-01-19 13:00:38 +0530

    [DEV] time in navbar

--- public/js/script.js
+++ public/js/script.js
@@ -39,3 +39,15 @@ document.addEventListener('DOMContentLoaded', function () {
         placeholderValue: "Select student"
     });
 });
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('clock').textContent = timeString;
+  }
+
+setInterval(updateClock, 1000);
+updateClock();
